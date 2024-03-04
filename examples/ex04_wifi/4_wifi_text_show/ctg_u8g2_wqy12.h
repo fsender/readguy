7bb2ba77f07289a99f1bd873a6e43987324deec1
fsender <f_ender@163.com>
2024-03-04 13:53:35 +0800

    1.3.5: add send-text example&fix bugs

---
+++ examples/ex04_wifi/4_wifi_text_show/ctg_u8g2_wqy12.h
@@ -0,0 +1,43 @@
+/******************** F r i e n d s h i p E n d e r ********************
+ * 本程序隶属于 Readguy 开源项目, 请尊重开源开发者, 也就是我FriendshipEnder.
+ * 如果有条件请到 extra/artset/reward 中扫描打赏,否则请在 Bilibili 上支持我.
+ * 项目交流QQ群: 926824162 (萌新可以进来问问题的哟)
+ * 郑重声明: 未经授权还请不要商用本开源项目编译出的程序.
+ * 
+ * @file ctg_u8g2_wqy12.h
+ * @author FriendshipEnder (f_ender@163.com), Bilibili: FriendshipEnder
+ * @version 1.0
+ * @date 2023-09-24
+ * @brief ReadGuy示例字体文件
+ * @attention
+ * Copyright (c) 2022-2023 FriendshipEnder
+ * 
+ * Apache License, Version 2.0
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+#ifndef _CTG_U8G2_WQY12_H_FILE
+#define _CTG_U8G2_WQY12_H_FILE
+#include <Arduino.h>
+#include <pgmspace.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+extern const uint8_t ctg_u8g2_wqy12_chinese1[14241] ;
+extern const uint8_t ctg_u8g2_wqy12_gb2312[208522] ;
+extern const uint8_t ctg_u8g2_wqy12[626234] ;
+#ifdef __cplusplus
+}
+#endif
+#endif
